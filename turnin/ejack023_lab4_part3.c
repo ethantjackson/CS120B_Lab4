a7e98ae04dd81991676e829583099b2d83a25cf1
Ethan Jackson <ejack023@wch136-24.cs.ucr.edu>
2021-04-16 20:26:06 -0700

    update part 3

--- turnin/ejack023_lab4_part3.c
+++ turnin/ejack023_lab4_part3.c
@@ -12,47 +12,58 @@
 #include "simAVRHeader.h"
 #endif
 
-enum SM_STATES {SM_Start, SM_Wait, SM_First, SM_Second, SM_Unlock, SM_Lock} SM_STATE;
+enum SM_STATES {SM_Start, SM_WaitFirst, SM_First, SM_WaitSecond, SM_Second, SM_Check, SM_Unlock, SM_Lock} SM_STATE;
 unsigned char prevInput = 0x00;
+unsigned char Inputs [2];
 void Tick_Fct() {
     switch(SM_STATE) {
 	case SM_Start:
-	    SM_STATE = SM_Wait;
+	    SM_STATE = SM_WaitFirst;
 	    break;
-	case SM_Wait: 
-	    if (PINA & 0x80 && !(PINA & 0x04 || PINA & 0x02 || PINA & 0x01)) SM_STATE = SM_Lock;
-	    else if (PINA) SM_STATE = SM_First;
+	case SM_WaitFirst: 
+	    if (PINA && PINA!=prevInput) SM_STATE = SM_First;
+	    else SM_STATE = SM_WaitFirst;
 	    break;
 	case SM_First:
-	    if (PINA & 0x80) SM_STATE = SM_Lock;
-	    else if (PINA & 0x04 && !(PINA & 0x02 || PINA & 0x01)) SM_STATE = SM_Second;
-	    else SM_STATE = SM_Wait;
+	    SM_STATE = SM_WaitSecond;
+	    break;
+	case SM_WaitSecond:
+	    if (PINA && PINA!=prevInput) SM_STATE = SM_Second;
+	    else SM_STATE = SM_WaitSecond;
 	    break;
 	case SM_Second:
-            if (PINA & 0x80) SM_STATE = SM_Lock;
-	    else if ((prevInput != 0x04 && !(PINA&0x04)) && PINA & 0x02 && !(PINA & 0x04 || PINA & 0x01)) SM_STATE = SM_Unlock;
-  	    else if ((prevInput != 0x04 && !(PINA&0x04)) && PINA) SM_STATE = SM_Wait;
+	    SM_STATE = SM_Check;
+	    break;
+	case SM_Check:
+            if (Inputs[0]==0x04 && Inputs[1]==0x02) SM_STATE = SM_Unlock;
+            else SM_STATE = SM_WaitFirst;	
 	    break;
 	case SM_Unlock:
-            if (PINA & 0x80) SM_STATE = SM_Lock;
-	    else if (prevInput != 0x02 && !(PINA&0x02)) SM_STATE = SM_Wait;
+	    if (PINA & 0x80) SM_STATE = SM_Lock;
+            else SM_STATE = SM_Unlock;
 	    break;
 	case SM_Lock:
-	    if (prevInput != 0x80 && !(PINA&0x80)) SM_STATE = SM_Wait;
+	    SM_STATE = SM_WaitFirst;
 	    break;
 	default:
-	    SM_STATE = SM_Wait;
+	    SM_STATE = SM_WaitFirst;
 	    break;
     }
     switch(SM_STATE) {
 	case SM_Start:
 	    break;
-        case SM_Wait:
+        case SM_WaitFirst:
 	    break;
 	case SM_First:
+	    Inputs[0] = PINA;
 	    break;
-	case SM_Second:
+	case SM_WaitSecond:
 	    break;
+	case SM_Second: 
+	    Inputs[1] = PINA;
+	    break;
+	case SM_Check: 
+ 	    break;
 	case SM_Unlock:
 	    PORTB = 0x01;
 	    break;
